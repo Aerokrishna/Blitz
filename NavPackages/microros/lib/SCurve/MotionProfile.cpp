602030e083829f681c84fed7f676fa1479e4461d
Krishnapranav <krishnapranav.221me227@nitk.edu.in>
2025-07-29 20:02:29 +0530

    pid ui serial tuning script works

--- NavPackages/microros/lib/SCurve/MotionProfile.cpp
+++ NavPackages/microros/lib/SCurve/MotionProfile.cpp
@@ -42,29 +42,18 @@ float MotionProfile::get_setpoint(unsigned long current_time) {
     elapsed_time = elapsed_time + dt;
 
     if (elapsed_time < t_acc) {
-        // PHASE 1
         curr_vel += max_acc * dt;
         current_point += curr_vel * dt;   
 
     } 
     else if (elapsed_time < (t_acc + t_consv)) {
-        // PHASE 2
-        // Serial.print("PHASE 2  ");
-        // Serial.println(current_point);
-
         curr_vel = max_vel;
         current_point += max_vel * dt;
-        
     } 
 
     else if (elapsed_time < (2 * t_acc + t_consv)) {
-        // PHASE 3
-        // Serial.print("PHASE 3 ");
-        // Serial.println(current_point);
-
         curr_vel = curr_vel - max_acc * dt;
         current_point += curr_vel * dt;
-
     } 
 
     else {
@@ -72,24 +61,11 @@ float MotionProfile::get_setpoint(unsigned long current_time) {
         // current_point = target_point;
     }
 
-    Serial.print(elapsed_time);
-    Serial.print(" prevtime  ");
-    Serial.print(prev_time);
-    Serial.print(" setpoint  ");
-    Serial.print(current_point);
-    Serial.print("target ");
-    Serial.print(target_point);
-    Serial.print("curr vel ");
-    Serial.println(curr_vel);
-    Serial.print("total time ");
-    Serial.println(total_time);
-    
     return current_point;
 }
 
 void MotionProfile :: reset_profile(float current, float target, float time){
     // resets all the variables for the motion profile
-    // prev_time = 0.0f;
     elapsed_time = 0.0f;
     curr_vel = 0.0f;
     current_point = current; // needs to be replaced with correct current_point with an argument
